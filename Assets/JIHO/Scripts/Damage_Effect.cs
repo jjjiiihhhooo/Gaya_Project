113e8348fdd49c1864443f82fffbfdc383cd2435
jjjiiihhhooo <thdwlgh950@naver.com>
2023-07-14 18:46:30 +0900

    이펙트

---
+++ Assets/JIHO/Scripts/Damage_Effect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage_Effect : MonoBehaviour
+{
+    public void Active()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
